9e366ee1818194e8e15aa47d423db3f0586eab43
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-30 23:00:01 +0530

    Initial commit

---
+++ src/test/java/com/friendsbook/functional/FriendsBookEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendsbook.functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsBookEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
