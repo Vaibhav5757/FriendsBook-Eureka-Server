41e2966c347be1b339cfc8c26182218545248315
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-06 21:06:20 +0530

    added annotation enable feign client

--- src/main/java/com/friendsbook/functional/FriendsBookEurekaServerApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookEurekaServerApplication.java
@@ -3,10 +3,12 @@ package com.friendsbook.functional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableFeignClients
 @ComponentScan(basePackages = {"com.friendsbook"})
 public class FriendsBookEurekaServerApplication {
 
