cf75c908818969f598932f1b87b83b9e2e64b5a1
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-06 21:40:01 +0530

    removed uneccessary code

--- src/main/java/com/friendsbook/util/ApplicationInitializer.java
+++ src/main/java/com/friendsbook/util/ApplicationInitializer.java
@@ -3,7 +3,6 @@ package com.friendsbook.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,22 +11,22 @@ import com.friendsbook.functional.UserServiceClient;
 @Component
 public class ApplicationInitializer implements CommandLineRunner{
 	
-//	@Value("${external-service-url.user-service}")
-//	private String userServiceUrl;
-//	
-//	@Value("${external-service-url.front-end}")
-//	private String frontEndSvcUrl;
 	
 	@Autowired
 	private UserServiceClient userClient;
 	
-	@SuppressWarnings("unused")
 	private Logger logger = LoggerFactory.getLogger(ApplicationInitializer.class);
 
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("Eureka Server Initialized");
-		this.userClient.wakeUp();
+		try {
+			String resp =  this.userClient.wakeUp();
+			if(resp != null)
+				logger.info("User Service woke up");
+		}catch (Exception err) {
+			logger.error(err.getMessage());
+		}
 	}
 
 }
