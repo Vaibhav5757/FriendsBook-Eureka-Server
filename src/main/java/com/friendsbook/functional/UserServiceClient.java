4ce7285f19a794471bab8922d50ae5dc04c82a1e
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-12-06 21:24:11 +0530

    debugging

--- src/main/java/com/friendsbook/functional/UserServiceClient.java
+++ src/main/java/com/friendsbook/functional/UserServiceClient.java
@@ -3,7 +3,7 @@ package com.friendsbook.functional;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(url = "${external-user-service}", name = "User-Service")
+@FeignClient(url = "${external-user-service.ribbon.listOfServers}", name = "User-Service")
 public interface UserServiceClient {
 
 	@GetMapping("/wake-up")
